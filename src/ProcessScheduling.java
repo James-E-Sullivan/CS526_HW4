bb545aebba8734a9c54daedcc30ffb1b24e1d207
James Sullivan <jesullivan222@gmail.com>
2019-04-14 20:25:38 -0400

    Functionality after D is empty added. Small bugs need to be fixed. Need to add file output functionality.

--- src/ProcessScheduling.java
+++ src/ProcessScheduling.java
@@ -6,9 +6,8 @@ import java.util.Scanner;
 
 public class ProcessScheduling {
 
-     private static HeapPriorityQueue readProcesses(){
+     private static HeapPriorityQueue<Integer, Process> readProcesses(){
         HeapPriorityQueue<Integer, Process> D = new HeapPriorityQueue<>();
-        //LinkedQueue<Process> processList = new LinkedQueue<>();
         String lineFromFile;
 
         try{
@@ -17,23 +16,10 @@ public class ProcessScheduling {
             Scanner fileInput = new Scanner(processFile);
 
             while (fileInput.hasNextLine()){            // loop through file lines
-
                 lineFromFile = fileInput.nextLine();    // get next line from file
 
                 // splits file line by space, into a String array
                 String[] splitLine = lineFromFile.split(" ");
-
-                /*
-
-                * Each splitLine element used as a Process parameter.
-                * Assumes process_scheduling_in.txt has 4 valid numbers
-                * in order of priority, id, arrivalTime, and duration.
-
-                Process p = new Process(Integer.valueOf(splitLine[0]),
-                        Integer.valueOf(splitLine[1]),
-                        Integer.valueOf(splitLine[2]),
-                        Integer.valueOf(splitLine[3]));
-                */
 
                 Process p = new Process();
                 p.setId(Integer.valueOf(splitLine[0]));
@@ -41,18 +27,7 @@ public class ProcessScheduling {
                 p.setDuration(Integer.valueOf(splitLine[2]));
                 p.setArrivalTime(Integer.valueOf(splitLine[3]));
 
-                /*
-                // test
-                System.out.println("Following process input into D:");
-                System.out.print("id: " + p.getId());
-                System.out.print(", priority: " + p.getPriority());
-                System.out.print(", duration: " + p.getDuration());
-                System.out.println(", arrivalTime: " + p.getArrivalTime() + "\n");
-
-                 */
-
                 D.insert(p.getArrivalTime(), p);
-                //processList.enqueue(p);     // add new Process to LinkedQueue
             }
         }
 
@@ -73,53 +48,71 @@ public class ProcessScheduling {
         return D;
     }
 
-    /*
-    private static void printProcess(Process p){
-         System.out.println("Process removed from queue is: id = " + p.getId() + ", at time ");
-    }
 
-     */
+    private static void printProcess(Process p, Integer currentTime, Integer waitTime, Double totalTime){
+         System.out.println("\nProcess removed from queue is: id = " + p.getId()
+                 + ", at time " + currentTime + ", wait time = " + waitTime +
+                 " Total wait time = " + totalTime);
+         System.out.println("Process id = " + p.getId());
+         System.out.println("        Priority = " + p.getPriority());
+         System.out.println("        Arrival = " + p.getArrivalTime());
+         System.out.println("        Duration = " + p.getDuration());
+    }
 
 
     public static void main(String[] args) {
 
-        //LinkedQueue<Process> processQueue = readProcesses();    // unchecked assignment warning
-        HeapPriorityQueue<Integer, Process> D = readProcesses();        // unchecked assignment warning
+        HeapPriorityQueue<Integer, Process> D = readProcesses();
         Integer currentTime = 0;    // current time set to 0 at start
         Boolean running = false;    // T if process running, otherwise F
-        HeapPriorityQueue<Integer, Process> Q = new HeapPriorityQueue();        // unchecked assignment warning
+        HeapPriorityQueue<Integer, Process> Q = new HeapPriorityQueue<>();
         Integer endTime = 0;
         Integer waitTime;
         Double totalWaitTime = 0.0;
         Process runningProcess = null;
+        Integer executionCount = 0;
 
         // start reading processQueue values into the heap priority queue
         while (!(D.isEmpty())){
 
-            Process p = D.min().getValue();
+            Process p = D.min().getValue();                         // process w/ min arrivalTime
             if (p.getArrivalTime() <= currentTime){
-                Process removedProcess = D.removeMin().getValue();
+                Process removedProcess = D.removeMin().getValue();  // process w/ min arrivalTime removed from D
                 removedProcess.setEntryTime(currentTime);
                 Q.insert(removedProcess.getPriority(),
-                        removedProcess);                                                 // unchecked assignment warning
+                        removedProcess);
             }
 
             if (!(Q.isEmpty()) && !running){
-                runningProcess = Q.removeMin().getValue();           // not sure if this has to be outside if statement
+                runningProcess = Q.removeMin().getValue();
 
                 // Get the process wait time and add it to totalWaitTime
                 waitTime = currentTime - runningProcess.getEntryTime();
                 totalWaitTime += waitTime;
+                printProcess(runningProcess, currentTime, waitTime, totalWaitTime);
+                running = true;
+                endTime = currentTime + runningProcess.getDuration();
+            }
 
-                System.out.println("\nProcess removed from queue is: id = " + runningProcess.getId()
-                        + ", at time " + currentTime + ", wait time = " + waitTime +
-                        " Total wait time = " + totalWaitTime);
-                System.out.println("Process id = " + runningProcess.getId());
-                System.out.println("        Priority = " + runningProcess.getPriority());
-                System.out.println("        Arrival = " + runningProcess.getArrivalTime());
-                System.out.println("        Duration = " + runningProcess.getDuration());
+            currentTime++;
 
+            if (running && currentTime.equals(endTime)){
+                running = false;
+                System.out.println("Process " + runningProcess.getId() +
+                        " finished at time " + currentTime);
+                executionCount++;
+            }
+        }
 
+        System.out.println("\nD becomes empty at time " + (currentTime - 1) + "\n");
+
+        while (!(Q.isEmpty())){
+
+            if (!running){
+                runningProcess = Q.removeMin().getValue();
+                waitTime = currentTime - runningProcess.getEntryTime();
+                totalWaitTime += waitTime;
+                printProcess(runningProcess, currentTime, waitTime, totalWaitTime);
 
                 running = true;
                 endTime = currentTime + runningProcess.getDuration();
@@ -131,41 +124,18 @@ public class ProcessScheduling {
                 running = false;
                 System.out.println("Process " + runningProcess.getId() +
                         " finished at time " + currentTime);
+                executionCount++;
             }
         }
 
-        while (!(Q.isEmpty())){
-
-            if (!running){
-                runningProcess = Q.removeMin().getValue();
-                System.out.print("Id = " + runningProcess.getId() + ", ");
-                System.out.print("priority = " + runningProcess.getPriority() + ", ");
-                System.out.print("duration = " + runningProcess.getDuration() + ", ");
-                System.out.println("arrival time = " + runningProcess.getArrivalTime());
-                running = true;
-                endTime = currentTime + runningProcess.getDuration();
-            }
-
-            currentTime++;
-
-            if (running && currentTime.equals(endTime))
-                running = false;
-
+        if (running && currentTime.equals(endTime)){
+            running = false;
+            System.out.println("Process " + runningProcess.getId() +
+                    " finished at time " + currentTime);
         }
 
+        System.out.println("\nTotal wait time = " + totalWaitTime);
+        System.out.println("Average wait time = " + (totalWaitTime / executionCount));
 
-
-
-        /*
-        // test
-        while (D.size() > 0) {
-            Process p = D.removeMin().getValue();
-            System.out.println("\n" + p.getPriority());
-            System.out.println(p.getId());
-            System.out.println(p.getArrivalTime());
-            System.out.println(p.getDuration());
-        }
-
-         */
     }
 }
